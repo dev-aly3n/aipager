62a7f8a941f4bd8c70b31e642e6c2342c26aeb24
dev-aly3n <dev.aly3n@gmail.com>
2026-08-23 10:14:53 +0000

    release 0.7.4

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
         aipager = python.pkgs.buildPythonApplication {
           pname  = "aipager";
           # Keep in sync with pyproject.toml [project].version.
-          version = "0.7.3";
+          version = "0.7.4";
           format = "pyproject";
 
           src = ./.;
